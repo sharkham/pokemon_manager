ee71baedf241dc012da398c8831b3050d72a0130
Sam Markham <sam.e.markham@gmail.com>
2019-10-03 12:47:48 -0700

    Adds seed file

---
+++ db/seeds.rb
@@ -0,0 +1,7 @@
+# Add seed data here. Seed your database with `rake db:seed`
+
+# Example from another lab:
+
+# sophie = Owner.create(name: "Sophie")
+# Pet.create(name: "Maddy", owner: sophie)
+# Pet.create(name: "Nona", owner: sophie)
