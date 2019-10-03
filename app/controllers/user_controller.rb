2b30e59b4466e85cddb041c0c0cb97e5db2f655d
Sam Markham <sam.e.markham@gmail.com>
2019-10-03 13:55:31 -0700

    Adds a post method for signup in the UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,4 +4,14 @@ class UserController < ApplicationController
     erb :signup
   end
 
+  post "/signup" do
+    user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
+    if user.save
+
+    else
+      redirect "/signup"
+      #one example redirects to failure; it would be better to go to sign-up again but with an error
+    end
+  end
+
 end
