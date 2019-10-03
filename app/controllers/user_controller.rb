0f0a1180e3e79a8b4a58a972d7759dcfe5e71f6a
Sam Markham <sam.e.markham@gmail.com>
2019-10-03 16:06:54 -0700

    Adds code and pseudocode to signup route in UserController

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,5 +1,7 @@
 class UserController < ApplicationController
 
+  # Signing up
+
   get "/signup" do
     erb :signup
   end
@@ -8,10 +10,13 @@ class UserController < ApplicationController
     user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
     if user.save
       #find that thing that validates the things.
+      #redirect to user being logged in
     else
       redirect "/signup"
       #one example redirects to failure; it would be better to go to sign-up again but with an error
     end
   end
+
+  # Logging in
 
 end
