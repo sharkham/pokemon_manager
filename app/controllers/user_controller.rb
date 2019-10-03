90a124b22c038ab17c8995459a90fc10597e8a40
Sam Markham <sam.e.markham@gmail.com>
2019-10-03 14:44:09 -0700

    Adds has_secure_password macro to User

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -7,7 +7,7 @@ class UserController < ApplicationController
   post "/signup" do
     user = User.new(:username => params[:username], :email => params[:email], :password => params[:password])
     if user.save
-
+      #find that thing that validates the things.
     else
       redirect "/signup"
       #one example redirects to failure; it would be better to go to sign-up again but with an error
