5a6a3a76e6a1203066aaa06169d8f425848026f8
Sam Markham <sam.e.markham@gmail.com>
2019-10-05 17:46:23 -0700

    Adds slug methods for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ActiveRecord::Base
 
   has_secure_password
   validates :username, :email, presence: true
+
+  def slug
+    self.username.gsub(" ", "-").downcase
+  end
+
+  def self.find_by_slug(slug)
+    self.all.find{ |instance| instance.slug == slug }
+  end
+
 end
