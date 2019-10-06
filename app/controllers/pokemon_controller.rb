0f28ae5d44d9aef135dbc60eed6acea849dca577
Sam Markham <sam.e.markham@gmail.com>
2019-10-05 17:46:00 -0700

    Adds Index route for Pokémon

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -3,7 +3,12 @@ class PokemonController < ApplicationController
   #Index
   get '/pokemon' do
     if logged_in?
-      @user
+      @user = current_user
+      @pokemon = current_user.pokemon
+      erb :'pokemon/index'
+    else
+      redirect '/login'
+    end
   end
 
   #Create
